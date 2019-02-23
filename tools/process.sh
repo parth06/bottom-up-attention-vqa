fec41370196d1ffb2b0810f9b3d5322edb806671
Parth Shah <parth.shah0096@gmail.com>
2019-02-23 08:16:01 -0500

    Update process.sh

--- tools/process.sh
+++ tools/process.sh
@@ -1,5 +1,6 @@
 # Process data
 
-python tools/create_dictionary.py
-python tools/compute_softscore.py
-python tools/detection_features_converter.py
+python2 tools/create_dictionary.py
+python2 tools/compute_softscore.py
+python2 tools/detection_features_converter.py
+python2 tools/detection_features_converter.py
