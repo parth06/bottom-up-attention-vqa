a1c4f89bc4f4238826eb8ac199520444693440e2
Parth Shah <parth.shah0096@gmail.com>
2019-02-23 08:09:38 -0500

    Update download.sh

--- tools/download.sh
+++ tools/download.sh
@@ -33,6 +33,6 @@ unzip data/trainval_36.zip -d data
 rm data/trainval_36.zip
 
 # Test Image Features
-wget -P data https://imagecaption.blob.core.windows.net/imagecaption/test2014_36.zip
-unzip data/test2014_36.zip -d data
-rm data/test2014_36.zip
+wget -P data https://imagecaption.blob.core.windows.net/imagecaption/test2015_36.zip
+unzip data/test2015_36.zip -d data
+rm data/test2015_36.zip
